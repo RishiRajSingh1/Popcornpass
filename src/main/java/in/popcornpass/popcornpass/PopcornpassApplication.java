111ab7ad7348fdc441fd06291d9d52a3dc2823c5
Rishi Raj Singh <rishiraj@lentra.ai>
2024-11-19 11:54:55 +0530

    Initial Commit For The Project

---
+++ src/main/java/in/popcornpass/popcornpass/PopcornpassApplication.java
@@ -0,0 +1,13 @@
+package in.popcornpass.popcornpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopcornpassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopcornpassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/popcornpass/popcornpass/PopcornpassApplicationTests.java
@@ -0,0 +1,13 @@
+package in.popcornpass.popcornpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopcornpassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
